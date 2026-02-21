2b57c39c3858d57634cdcd41b66d8d84a6bbc767
Hades398 <sammyboy102803@gmail.com>
2026-02-20 20:20:49 -0700

    Added header component with link to documentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { DocumentEditorContainerComponent } from '@syncfusion/ej2-react-documenteditor'
-
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+        <Header />
+
         <DocumentEditorContainerComponent height='900px'></DocumentEditorContainerComponent>
+
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="header-center">
+                <h1>SOAP Notes Editor</h1>
+            </div>
+            <div className="header-docs-link">
+                <a href="https://423s26.github.io/project6/">
+                    <h3>
+                        Need Help?
+                    </h3>
+                </a>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
