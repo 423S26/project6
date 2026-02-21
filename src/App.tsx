978a284fbdf8cc110faa8d8b7a242b867ae7563f
Caitlin <chermanson03@gmail.com>
2026-02-20 18:58:00 -0700

    new word processor files without Springboot, deleted empty document_editor folder

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { DocumentEditorContainerComponent } from '@syncfusion/ej2-react-documenteditor'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DocumentEditorContainerComponent height='900px'></DocumentEditorContainerComponent>
+    </div>
+  );
+}
+
+export default App;
