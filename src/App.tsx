5e65e0a3f01ade0ac0ca695365b42b15e52bed85
Caitlin <chermanson03@gmail.com>
2026-02-20 19:44:47 -0700

    modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <DocumentEditorContainerComponent height='900px'></DocumentEditorContainerComponent>
+        <DocumentEditorContainerComponent height='900px'></DocumentEditorContainerComponent>
     </div>
   );
 }
