3c0fe603050c2f5d7b8d91dbd0963a09e87c4339
Caitlin <chermanson03@gmail.com>
2026-03-02 20:25:46 -0700

    modified TextBox.tsx to add a toolbarand added a save button which has bugs

--- src/components/SOAPBox/TextBox.tsx
+++ src/components/SOAPBox/TextBox.tsx
@@ -1,18 +1,24 @@
 import React from "react";
+import { useRef } from 'react';
 import "./TextBox.css";
-import { DocumentEditorContainerComponent } from '@syncfusion/ej2-react-documenteditor'
+import { DocumentEditorContainerComponent, Toolbar } from '@syncfusion/ej2-react-documenteditor'
 
 interface Title {
     title : string
 }
-
+DocumentEditorContainerComponent.Inject(Toolbar);
 function TextBox({ title }: Title) {
+    let container = useRef<DocumentEditorContainerComponent>(null);
+    function save() {
+        container.current.documentEditor.save('sample', 'Docx');
+    }
     return (
         <div className="textbox">
             <h2 className="title">
                 { title }
             </h2>
             <DocumentEditorContainerComponent width="1200px"></DocumentEditorContainerComponent>
+            <button onClick={save}>Save</button>
         </div>
     );
 }
