93c76eeb772729c48825bbcb059071f717dc05a7
Hades398 <sammyboy102803@gmail.com>
2026-03-27 03:49:02 -0600

    More playwright test fixes

--- src/tests/open_button_test.spec.ts
+++ src/tests/open_button_test.spec.ts
@@ -3,11 +3,11 @@ import { test, expect } from '@playwright/test';
 test('should open the file browser when the open button is clicked', async ({ page }) => {
   await page.goto('https://project6-ten-zeta.vercel.app/');
 
-  await page.getByRole('button', { name: 'General Notes' }).click();
+  await page.waitForSelector('.e-documenteditorcontainer');
+
+  await page.getByRole('button', { name: /General Notes/i }).click();
 
   const openButton = page.locator('button[title="Open"]');
-  
-  await openButton.click();
 
   const fileChooserPromise = page.waitForEvent('filechooser');
   await openButton.click();
