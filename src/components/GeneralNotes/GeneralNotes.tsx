408b5240c45b208cdc29c1ce83121f14d131485d
Hades398 <sammyboy102803@gmail.com>
2026-04-10 17:13:58 -0600

    Removed unused css file.

--- src/components/GeneralNotes/GeneralNotes.tsx
+++ src/components/GeneralNotes/GeneralNotes.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import ReactQuill from "react-quill-new";
 import "react-quill-new/dist/quill.snow.css";
-import "./GeneralNotes.css";
 
 interface GeneralNotesProps {
     editorRef: React.RefObject<any>;
